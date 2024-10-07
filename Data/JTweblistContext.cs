6cde7de4d3577bb8e494d63bb9f12df669289324
laboratorio <laboratorio@UPEAULA-31714>
2024-10-07 11:19:03 -0500

    Agregar archivos de proyecto.

---
+++ Data/JTweblistContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JTweblist.Models;
+
+    public class JTweblistContext : DbContext
+    {
+        public JTweblistContext (DbContextOptions<JTweblistContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JTweblist.Models.Burger> Burger { get; set; } = default!;
+    }
